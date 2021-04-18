f21c322b24d767a88471ffbeaab6b0f7a15f2aa3
philmon43 <philmon43@gmail.com>
2021-04-18 19:08:59 +0300

    git initial commit

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const App = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get("http://localhost:5000");
+      setData(data)
+    }
+
+    fetchData()
+  }, []);
+  
+  return (
+    <div>
+      {data && data.map(data => {
+        return <div key={data.firstName + data.lastName}>
+          <p>{data.firstName} {data.lastName} </p>
+        </div>
+      })}
+    </div>
+  )
+}
+
+export default App;
